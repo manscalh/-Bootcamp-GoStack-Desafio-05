5e47de96a6fc2f04101b2b0e276a4e74841ae9fc
Salomao Calheiros <salomao.calheiros@gmail.com>
2021-01-06 12:57:59 -0400

    sparkles: challenge finalized

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transaction = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({transaction,balance});
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +20,12 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+
+    const { title, value, type } = request.body;
+    const createTransactonService = new CreateTransactionService(transactionsRepository);
+    const transaction = createTransactonService.execute({title,value,type});
+    return response.status(201).json(transaction);
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
